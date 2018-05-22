299d440c119001fdd1acd33359186d564a6a3161
csc <506395460@qq.com>
2018-05-22 15:59:45 +0800

    Initial commit

---
+++ viap-web/src/main/java/com/harzone/viap/web/TestController.java
@@ -0,0 +1,28 @@
+package com.harzone.viap.web;
+
+import com.harzone.viap.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    private TestService testService;
+
+//    @RequestMapping(value = "/add", method = RequestMethod.GET)
+//    public String add() {
+//        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+//    }
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return testService.addService();
+    }
+
+}
